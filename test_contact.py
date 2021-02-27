242879216b62853b351a4ec8a3f6ebaa78b4687c
panda-123 <herui5555@126.com>
2021-02-27 11:15:22 +0800

    jenkins test

---
+++ test_contact.py
@@ -0,0 +1,14 @@
+# coding=UTF-8
+# 作者:herui
+# 时间:2021/1/28 20:50
+# 功能: 接口测试
+
+
+import requests
+def test_req():
+    # 获取access_token的值
+    ID = 'ww9db5398ae0670287'
+    SECRET = 'rjGnaO2DrF-dUVycJOd7QzoikdKKXzBVG8vUi8lSNSk'
+    res = requests.get(f'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={ID}&corpsecret={SECRET}')
+    access_token = res.json()['access_token']
+    print(res.json(),'\n', access_token)
